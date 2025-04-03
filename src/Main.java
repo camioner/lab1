42394e51ab70ceab61fdd51f40853ebc8a636f98
camioner <cris_to515@hotmail.com>
2025-04-03 17:15:41 -0300

    avances2

--- src/Main.java
+++ src/Main.java
@@ -2,64 +2,83 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
-//public class BigVigenere {
-//    private int[] key;
-//    private char [][] alphabet;
-//    // private static final String code = "abcdefghijklmnñopqrstuvwxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-//    // private static final int LargoCode = code.length();
-//
-//    public BigVigenere(){
-//        static scanner = new scanner (system.in);
-//        system.out.print ("ingrese la key");
-//        setKey(scanner.nextline());
-//    }
-//
-//
-//
-//    public BigVigenere(String numericKey){
-//        int largo = numericKey.length();
-//        //Constructor que recibe la clave
-//        key = new int[largo];
-//
-//        for (int i=0; i<largo; i++ ){
-//            key[i] = numericKey.ParseInt() / 10^(largo-1);
-//            largo--;
-//        }
-//        //numérica como una cadena y la almacena en el arreglo key. Y también genera la
-//        //matriz del alfabeto para el algoritmo Vigenère.
-//    }
-
 class BigVigenere {
     private int key[];
-    private char[][] alphabet= new char[64][64];
-    String alphanumeric = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private char[][] alphabet = new char[64][64];
+    String alphanumeric = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ0123456789";
 
     public BigVigenere() {
         System.out.println(" ingrese la clave ");
-        Scanner input= new Scanner(System.in);
-        String Skey=input.nextLine();
+        Scanner input = new Scanner(System.in);
+        String Skey = input.nextLine();
+        System.out.println(" ingrese el mensaje a cifrar ");
+        input = new Scanner(System.in);
+        String mnsj = input.nextLine();
         input.close();
-        this.key= new int[Skey.length()];
-        //llenamois key[] con la clave por separado
+
+        this.key = new int[Skey.length()];
         for (int i = 0; i < Skey.length(); i++) {
             this.key[i] = Character.getNumericValue(Skey.charAt(i));
         }
-        //esto llena alphabet con alphanumeric en forma de espiral , es una matrisz de 64x64
+
+// llenamois key[] con la clave por separado
+// for (int i = 0; i < Skey.length(); i++) {
+// this.key[i] = Character.getNumericValue(Skey.charAt(i));
+// }
+// esto llena alphabet con alphanumeric en forma de espiral , es una matrisz de 64x64
         for (int i = 0; i < alphanumeric.length(); i++) {
             for (int j = 0; j < alphanumeric.length(); j++) {
                 alphabet[i][j] = alphanumeric.charAt((i + j) % alphanumeric.length());
             }
         }
-        input.close();
+        new BigVigenere(mnsj);
 
 
     }
-    public BigVigenere(String numericKey){
+
+    public BigVigenere(String numericKey) {
+// Constructor que recibe la clave en forma de String y
+// luego la transforma a la clave númerica correspondiente para almacenarla en el atributo key. También
+// genera la matriz del alfabeto para el algoritmo Vigenère
 
         //llenamois key[] con la clave por separado
-        for (int i = 0; i < Skey.length(); i++) {
-            this.key[i] = Character.getNumericValue(Skey.charAt(i));
+        for (int i = 0; i < numericKey.length(); i++) {
+            this.key[i] = Character.getNumericValue(numericKey.indexOf(i));
         }
+    }
+
+    public String encrypt(String message) {
+// Método que cifra un mensaje utilizando la clave numérica
+// almacenada y la matriz del alfabeto usando el algoritmo de Vigenère
+        char[] encrypted = new char[message.length()];
+        for (int i = 0; i < message.length(); i++) {
+            encrypted[i] = alphabet[findPos(message.charAt(i), 0)][key[i % key.length]];
+        }
+        return encrypted.toString();
+    }
+
+    public String decrypt(String encryptedMessage) {
+        StringBuilder resultado = new StringBuilder();
+        int largo = key.length;
+        for (int i = 0; i < encryptedMessage.length(); i++) {
+            int pos = key[i % largo];
+            int basepos = findPos(encryptedMessage.charAt(i), pos);
+            resultado.append(alphabet[0][basepos]);
+        }
+        return resultado.toString();
+    }
+
+    public void reEncrypt(String encryptedMessage) {
+
+    }
+
+    private int findPos(char target, int e) {
+        for (int i = 0; i < 64; i++) {
+            if (alphabet[e][i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
